edccda697c8aef7929ee1b2efddff67b43b4d6a6
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-07-21 15:40:15 +0700

    add State interface

---
+++ src/State.java
@@ -0,0 +1,13 @@
+/*
+ * Name:    Supakrit Pamakama
+ * ID:      6510450968
+ */
+
+public interface State {
+    public void insertQuarter();
+    public void ejectQuarter();
+
+    public void turnCrank();
+
+    public void dispense();
+}
